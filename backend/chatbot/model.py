87624b413b11b9fbfaf88cd642d1c063a97c0598
himaja-self <himajashree.0123@gmail.com>
2025-07-08 20:05:46 +0530

    Changed the model from gemini to mistral from huggingface_hub

--- backend/chatbot/model.py
+++ backend/chatbot/model.py
@@ -1,8 +1,13 @@
 import google.generativeai as genai
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
 
 # Set your API key (better to load this from .env in production)
-genai.configure(api_key="AIzaSyDD0PvL3a5UpE58vz-LEwh_VWzS_A3aS4g")
+genai.configure(api_key=gemini_api_key)
 
 # Load the Gemini model
 model = genai.GenerativeModel("gemini-2.5-flash")
